7ae876d658b467eddc5e080202fef335f80ab77e
ReemDa03 <reemdarwish07@gmail.com>
2025-07-05 08:34:06 +0300

    Fix CORS for Railway

--- frontend/src/Pages/Cart/CartPage.jsx
+++ frontend/src/Pages/Cart/CartPage.jsx
@@ -49,7 +49,7 @@ const CartPage = () => {
       return;
     }
 
-    const res = await axios.post("http://localhost:5000/create-checkout-session", {
+    const res = await axios.post("https://reverse-production.up.railway.app/create-checkout-session", {
       total: total,
       currency: restaurantData.currency || "usd", // حسب العملة من Firestore
       slug: slug, // نبعته عشان السيرفر يجيب بياناته لحاله
